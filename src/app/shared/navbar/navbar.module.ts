69a42c57dcf769c10ce63c9cb99ea6fa9c3af343
Ubiratan Leitão Mourão <ubiratan.mourao@gmail.com>
2019-11-07 00:20:06 -0300

    Criação do botão logout para a destrição do token guardado

--- src/app/shared/navbar/navbar.module.ts
+++ src/app/shared/navbar/navbar.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
 import { NavbarComponent } from './navbar.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TokenStorageService } from 'app/auth/token-storage.service';
 
 @NgModule({
     imports: [ RouterModule, CommonModule, NgbModule ],
@@ -10,4 +11,5 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     exports: [ NavbarComponent ]
 })
 
-export class NavbarModule {}
+export class NavbarModule {
+}
