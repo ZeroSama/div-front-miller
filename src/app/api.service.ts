f098a9afe88ebc8fc2d796aadbd0d4da2b0885e7
Ubiratan Leitão Mourão <ubiratan.mourao@gmail.com>
2019-11-10 15:42:03 -0300

    Criação dos metodos para comunicação da api com os resources de categoria de gasto

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AuthLoginInfo } from 'app/auth/auth-login-info';
 import { SignUpInfo } from 'app/auth/sign-up-info';
+import { Observable } from 'rxjs';
+import { CategoriaGasto } from './model/categoria-gasto';
 
 @Injectable({
   providedIn: 'root'
@@ -19,5 +21,17 @@ apiURL = 'http://localhost:8080';
     cadastraUsuario(usuario: any) {
     const url = `${this.apiURL}/usuarios`;
     return this.http.post<SignUpInfo>(url, usuario);
+  }
+  listCategoriaDeGasto() {
+    const url = `${this.apiURL}/categorias-de-gasto`;
+    return this.http.get<CategoriaGasto>(url, { responseType: 'json' });
+  }
+  cadastroCategoriaGasto(categoriaGasto: CategoriaGasto) {
+    const url = `${this.apiURL}/categorias-de-gasto`;
+    return this.http.post<CategoriaGasto>(url, categoriaGasto);
+  }
+  excluirCategoriaGasto(cod: any) {
+    const url = `${this.apiURL}/categorias-de-gasto/${cod}`;
+    return this.http.delete<any>(url);
   }
 }
