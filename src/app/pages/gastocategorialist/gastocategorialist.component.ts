2fa7da9d4b77e4778ea05cd397152b7d8433231d
Ubiratan Leitão Mourão <ubiratan.mourao@gmail.com>
2019-11-10 15:42:40 -0300

    alterações na lista de categoria de gasto para que ela possa listar os gastos cadastrados e recebidos da api

--- src/app/pages/gastocategorialist/gastocategorialist.component.ts
+++ src/app/pages/gastocategorialist/gastocategorialist.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'app/api.service';
+import { CategoriaGasto } from 'app/model/categoria-gasto';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-gastocategorialist',
@@ -7,9 +10,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GastocategorialistComponent implements OnInit {
 
-  constructor() { }
+  categoriasGasto: CategoriaGasto[];
+  key = 'descricao'; // Define um valor padrão, para quando inicializar o componente
+  reverse = false;
+
+  constructor(private apiService: ApiService, private router: Router) { }
 
   ngOnInit() {
+    this.categoriasGasto = new Array();
+    this.listCategoriaGasto();
   }
+  listCategoriaGasto() {
+    this.apiService.listCategoriaDeGasto().subscribe(
+      data => {
+        console.log('primeiro');
+        console.log(data);
+        this.categoriasGasto = data as unknown as CategoriaGasto[];
+        console.log('segundo');
+        console.log(this.categoriasGasto);
+      },
+      error => {
+        console.log(error);
+      }
+    )
 
+  }
+  goToCadastro() {
+    this.router.navigate(['/catgasto']);
+  }
+  excluir(categoriaGasto: CategoriaGasto) {
+    const resposta = confirm('Deseja realmente excluir a categoria \'' + categoriaGasto.descricao + '\'');
+    if (resposta) {
+      this.apiService.excluirCategoriaGasto(categoriaGasto.cod).subscribe(
+        data => {
+          window.location.reload();
+        },
+        error => {
+          console.log(error);
+        }
+      )
+    }
+  }
 }
